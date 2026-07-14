abad0fc727e4d6575834d1424ab853f5074c1007
Areco892 <ahuangliu@ucsd.edu>
2026-07-14 11:16:41 -0700

    Added linting.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,17 +1,21 @@
-import pytest
 from calculator.core import add, subtract, multiply, divide
+
 
 def test_add():
     assert add(2, 3) == 5
 
+
 def test_subtract():
     assert subtract(3, 2) == 1
+
 
 def test_multiply():
     assert multiply(2, 3) == 6
 
+
 def test_divide():
-    assert divide(2, 3) == 2/3
+    assert divide(2, 3) == 2 / 3
+
 
 def test_divide_by_zero():
     assert divide(5, 0) == "undefined"
